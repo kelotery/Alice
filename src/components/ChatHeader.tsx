ae35f1e216900a2070e66e5967f17e037da567ea
kelotery <78081881+kelotery@users.noreply.github.com>
2026-06-19 01:44:41 +0800

    Delete src directory

--- src/components/ChatHeader.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useConversation } from "../store/conversationStore";
-
-const ChatHeader: React.FC = () => (
-  <div className="chat-header">与Marvis的对话</div>
-);
-
-export default ChatHeader;
